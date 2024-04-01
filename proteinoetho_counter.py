e76f8dbb66f6746fca4ba9f6b92e383fafe373a8
Gyanesh Shukla <142218807+shuklagyanesh21@users.noreply.github.com>
2024-04-01 12:14:43 +0530

    proteinoetho_counter.py now has functin to determine core, accesory or unique genes

--- proteinoetho_counter.py
+++ proteinoetho_counter.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 
-#Take user input for the tsv file
 file_path = input("Enter the path to proteinortho result tsv file: ")
 if not os.path.exists(file_path):
     print("File does not exist.")
@@ -12,21 +11,33 @@ df = pd.read_csv(file_path, delimiter="\t")
 
 # Function to replace values based on conditions
 def replace_value(cell_value):
-    if "," in str(cell_value):
-        comma_count = cell_value.count(",") # Count the number of commas
-        replaced_value = "P" * (comma_count + 1) # Replace with "P" repeated (comma count + 1) times
+    if "," in str(cell_value): 			 # For organisms with multiple genes
+        comma_count = cell_value.count(",") 
+        replaced_value = "P" * (comma_count + 1) # Genes are 'one' more than the comma number
     elif "__peg_" in str(cell_value):
-        replaced_value = "P" # Replace with "P" if cell contains "__peg_"
+        replaced_value = "P" 			 # Now only single gene organisms left so extract any word
     elif cell_value == "*":
-        replaced_value = "A" # Replace "*" with "A"
+        replaced_value = "A"
     else:
-        replaced_value = cell_value # No replacement needed
+        replaced_value = cell_value 		 # No replacement needed
     return replaced_value
-
-# Apply the function to cells in the dataframe
+    
+# Apply the function to columns one-by-one
 for column in df.columns:
     df[column] = df[column].apply(replace_value)
     
+# Function to determine category of genes
+def determine_category(row):
+    if "A" not in row.values:
+        return "core"
+    elif row.tolist().count("P") == 1:  # convert rows to list then count P
+        return "unique"
+    else:
+        return "accessory"
+
+# Apply the function row-wise to create the 'category' column
+df['category'] = df.apply(determine_category, axis=1)
+
 # Write the modified dataframe to a new TSV file
 output_file_path = os.path.splitext(file_path)[0] + "_modified.tsv"
 df.to_csv(output_file_path, sep="\t", index=False)
@@ -34,5 +45,4 @@ df.to_csv(output_file_path, sep="\t", index=False)
 # Print a message confirming the write operation
 filename = os.path.basename(file_path)
 outname = os.path.basename(output_file_path)
-
 print(filename + " has been modified and saved as " + outname)
